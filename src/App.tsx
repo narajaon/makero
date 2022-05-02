1d5ab2e8d349ede52bd015830cf68964af278699
narajaon <rajaonarisonfabien@yahoo.fr>
2022-05-02 23:32:20 +0200

    handle 'eth_accounts' and 'eth_requestAccounts' methods

--- src/App.tsx
+++ src/App.tsx
@@ -2,44 +2,65 @@ import React, { useCallback, useEffect, useState } from "react";
 import detectEthereumProvider from "@metamask/detect-provider";
 import "./App.css";
 
-function useProvider() {
+function useEthProvider() {
   const [metamaskProvider, setProvider] = useState(null) as any;
-  const [account, setAccount] = useState(null) as any;
+  const [account, setAccount] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const requestAccountsCB = useCallback(async () => {
-    if (metamaskProvider && !account) {
-      setIsLoading(true);
-      const accounts = await metamaskProvider.request({
-        method: "eth_requestAccounts",
-      });
-      setIsLoading(false);
+  const requestAccountsCB = useCallback(
+    async (method: string) => {
+      if (!metamaskProvider) return;
 
-      setAccount(accounts[0]);
-    }
-  }, [metamaskProvider, account]);
+      try {
+        setIsLoading(true);
+        const accounts = await metamaskProvider.request({
+          method,
+        });
+        setIsLoading(false);
+
+        if (accounts.length === 0) {
+          return;
+        }
+
+        setAccount(accounts[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [metamaskProvider]
+  );
 
   useEffect(() => {
     (async () => {
-      const provider = (await detectEthereumProvider()) as any;
+      try {
+        const provider = (await detectEthereumProvider()) as any;
 
-      if (provider) {
-        setProvider(provider);
+        if (provider) {
+          setProvider(provider);
+          requestAccountsCB("eth_accounts");
+        }
+      } catch (error) {
+        console.log(error);
       }
     })();
-  }, []);
+  }, [requestAccountsCB]);
 
   return { requestAccountsCB, account, isLoading };
 }
 
 function App() {
-  const { requestAccountsCB, account, isLoading } = useProvider();
-  console.log({ account, isLoading });
+  const { requestAccountsCB, account, isLoading } = useEthProvider();
 
   return (
     <div className="App">
       {!account ? (
-        <button onClick={requestAccountsCB} disabled={isLoading}>
+        <button
+          onClick={async () => {
+            await requestAccountsCB("eth_requestAccounts");
+            window.location.reload();
+          }}
+          disabled={isLoading}
+        >
           {isLoading ? "LOADING" : "CONNECT YOUR WALLET"}
         </button>
       ) : (
